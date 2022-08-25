14b33bfefe3ccd9141d4885b3cf35fbd94b3aac5
CelesticCS <samaysssk@gmail.com>
2022-08-25 09:48:39 +0530

    Basic Isprime code

---
+++ 1.ts
@@ -0,0 +1,12 @@
+function isprime(num:number):boolean
+{
+    for(let i=2;i<num;i++)
+    {
+        if(num%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(isprime(10));
